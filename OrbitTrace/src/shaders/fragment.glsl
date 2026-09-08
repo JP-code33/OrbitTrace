d7ef3a8547278aab9ea381f486c8134564e48907
JP-code33 <pateljhenil@gmail.com>
2026-09-08 15:58:11 -0400

    tried to add like day/night but it doesn't work yet and will have to change the code again

--- OrbitTrace/src/shaders/fragment.glsl
+++ OrbitTrace/src/shaders/fragment.glsl
@@ -1,11 +1,15 @@
 uniform sampler2D globeTexture;
 varying vec2 vertexUV;
 varying vec3 vertexNormal;
+uniform vec3 sunDirection;
 
 void main() {
     float intensity = 1.05 - dot(vertexNormal, vec3(0.0, 0.0, 1.0));
     vec3 atmosphere = vec3(0.3, 0.6, 1.0) * pow(intensity, 1.5);
 
-    gl_FragColor = vec4(atmosphere + 
-    texture2D(globeTexture, vertexUV).xyz, 1.0);
+    float sunLight = dot(vertexNormal, sunDirection);
+    vec3 earthColor = texture2D(globeTexture, vertexUV).xyz;
+    float dayNight = smoothstep(-0.2, 0.2, sunLight);
+    earthColor *= mix(0.15, 1.0, dayNight);
+    gl_FragColor = vec4(atmosphere + earthColor, 1.0);
 }
