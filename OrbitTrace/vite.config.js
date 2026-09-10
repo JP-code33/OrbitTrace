b74e4f003260be4f9f79e4eeeb9ea70772e5fd12
JP-code33 <pateljhenil@gmail.com>
2026-09-10 17:32:48 -0400

    fixed the vite.config

--- OrbitTrace/vite.config.js
+++ OrbitTrace/vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
     },
     resolve: {
         alias: {
-            "satellite.js": path.resolve(__dirname, "./node_modules/satellite.js/dist/satellite.es.js")
+            "satellite.js": path.resolve(import.meta.dirname, "./node_modules/satellite.js/dist/satellite.es.js")
         }
     }
 })
